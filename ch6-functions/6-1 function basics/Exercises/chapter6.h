e93544cf17fb8f793e3be09b2129bc01563df496
youssief101 <30203051305119@sci.asu.edu.eg>
2023-06-12 11:42:28 +0300

    finish section 1 chapter 6

---
+++ ch6-functions/6-1 function basics/Exercises/chapter6.h
@@ -0,0 +1,5 @@
+// function that takes an integer and returns the factorial n!
+int fact(int i);
+
+// a function that returns the absolute value of an argument
+double absolute(double i);
---
+++ ch6-functions/6-1 function basics/Exercises/exercise6-8.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using std::cout; using std::endl;
+#include "chapter6.h"
+
+int main() {
+    int value = fact(5);
+    cout << value << endl;
+    return 0;
+}
